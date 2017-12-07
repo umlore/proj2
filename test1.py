013a2fb28b0e3b3fba8532eb08bef2bd65ceb1fd
Matthew <chuangmatthew@yahoo.com>
2017-12-06 20:20:53 -0500

    added file write functionality to test file

--- test1.py
+++ test1.py
@@ -9,3 +9,8 @@ the_headers = {"Content-Type": "application/json"}
 inverted_index_json = requests.post('http://teamthorn.cs.rpi.edu:5000/ranking', data=index_json_request, headers=the_headers, timeout=1.000)
 
 print(json.loads(inverted_index_json.content))
+
+f = open('testing.txt','w')
+f.write(json.loads(inverted_index_json.content))
+
+f.close()
