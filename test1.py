f4069d232bdf78ea808213a409edd85d0ef87796
Kevin <deleok@rpi.edu>
2017-12-06 20:11:17 -0500

    added working test file for ranking

---
+++ test1.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+import requests
+import json
+
+
+index_json_request = '{"query":["hello", "hi"],"ngrams":["hello"],"aliases":["hello","hell"]}'
+the_headers = {"Content-Type": "application/json"}
+
+inverted_index_json = requests.post('http://teamthorn.cs.rpi.edu:5000/ranking', data=index_json_request, headers=the_headers, timeout=1.000)
+
+print(json.loads(inverted_index_json.content))
