671de8d6327001cfc8d8203ad8b1cc46fc824eed
Matthew <chuangmatthew@yahoo.com>
2017-12-08 10:45:23 -0500

    add test case 2 for parseQueryJson

--- test_input_parse.py
+++ test_input_parse.py
@@ -8,15 +8,18 @@ from rank import parseQueryJson, parseIndexTermsFromQuery
 #Testing for input parsing from other groups
 #Calls will be made directly from rank.py functions ignoring POST and GET calls
 
+f = open('testing_input_parse.txt','w')
+
 '''
 THIS TEST BLOCK IS FOR TESTING THE parseQueryJson METHOD
 
 '''
-f = open('testing_input_parse.txt','w')
 f.write("RESULTS FOR TESTING parseQueryJson: \n")
 
+# ==============================
 #TEST 1: Testing basic query
 #Create dummy data (dictionary) and jsonify
+# ==============================
 query_dict = {
    'search_id': 123,
    'raw':
@@ -43,6 +46,37 @@ json_equals_dict = query_dict == parse_query_result
 
 #Write pass/fail results to output file
 f.write("----- Test 1 -----\nTest Passed: %r\n" % json_equals_dict) 
+
+# ==============================
+#TEST 2: Testing an empty query
+# ==============================
+#Create dummy data (dictionary) and jsonify
+query_dict = {
+   'search_id': 123,
+   'raw':
+   {
+       'raw_search': "",
+       'raw_tokens': []
+   },
+   'transformed':
+   {
+       'transformed_search': "",
+       'transformed_tokens': [],
+       'transformed_bigrams': [],
+       'transformed_trigrams': [],
+   }
+}
+
+json_query = jsonify(query_dict)
+
+#Pass it into the parsing method
+parse_query_result = parseQueryJson(json_query)
+
+#Compare output to original input dictionary to make sure they are the same
+json_equals_dict = query_dict == parse_query_result
+
+#Write pass/fail results to output file
+f.write("----- Test 2 -----\nTest Passed: %r\n" % json_equals_dict) 
 
 
 f.close()
