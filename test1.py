94fa98bddc4ae6c00e45257b445c0e5462b747bf
Eugene Umlor <umlore@rpi.edu>
2017-12-08 09:53:33 -0500

    cleaned up comments a bit

--- test1.py
+++ test1.py
@@ -2,8 +2,6 @@ from flask import Flask, request, jsonify
 import requests
 import json
 
-
-#index_json_request = '{"query":["hello", "hi"],"ngrams":["hello"],"aliases":["hello","hell"]}'
 the_headers = {"Content-Type": "application/json"}
 
 updated_query_request = '{"search_id":69,"raw":{"raw_search":"query test example","raw_tokens":["query","test","example"]},"transformed":{"transformed_search":"what even goes here","transformed_tokens":["what","even","goes","here"],"transformed_bigrams":["query test","test example"],"transformed_trigrams":["query test example"]}}'
