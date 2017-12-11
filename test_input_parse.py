0078c0350077f3779fbb61ad61d29fa2b449675a
Matthew <chuangmatthew@yahoo.com>
2017-12-10 19:01:13 -0500

    Finished writing test cases for parseJsonFromIndex

--- test_input_parse.py
+++ test_input_parse.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 #Import parsing functions to test
-from rank import parseQueryJson, parseIndexTermsFromQuery
+from rank import parseQueryJson, parseIndexTermsFromQuery, parseJsonFromIndex
 
 #Testing for input parsing from other groups
 #Calls will be made directly from rank.py functions ignoring POST and GET calls
@@ -182,5 +182,115 @@ result_equals_list = None == parse_query_result
 
 #Write pass/fail results to output file
 f.write("----- Test 3 -----\nTest Passed: %r\n" % result_equals_list) 
+
+
+
+
+'''
+THIS TEST BLOCK IS FOR TESTING THE parseJsonFromIndex METHOD
+
+'''
+f.write("RESULTS FOR TESTING parseIndexTermsFromQuery: \n")
+
+# ==============================
+# TEST 1: Testing basic query 
+# ==============================
+
+# Create dummy data (dictionary)
+query_dict = {
+    "returnCode": 0,
+    "error": "success",
+    "documents": [
+        {
+            "documentID": "dumbnerds.com",
+            "wordCount": 9001,
+            "pageLastIndexed": "1984",
+            "importantTokenRanges": [
+                {
+                    "fieldName": "dank",
+                    "rangeStart": 1,
+                    "rangeEnd": 666
+                },
+                {
+                    "fieldName": "memes",
+                    "rangeStart": 10000,
+                    "rangeEnd": 666000
+                }
+            ]
+        }
+    ],
+    "tokens": [
+        {
+            "token": "dank",
+            "ngramSize": 1,
+            "documentOccurences": [
+                {
+                    "documentID": "dumbnerds.com",
+                    "locations": [1,666]
+                }
+            ]
+        },
+        {
+            "token": "memes",
+            "ngramSize": 1,
+            "documentOccurences": [
+                {
+                    "documentID": "dumbnerds.com",
+                    "locations": [10000,666000]
+                }
+            ]
+        }
+    ]
+}
+
+json_query = query_dict
+
+#Pass it into the parsing method
+parse_query_result = parseJsonFromIndex(json_query)
+
+#Compare output to original input dictionary to make sure they are the same
+json_equals_dict = query_dict == parse_query_result
+
+#Write pass/fail results to output file
+f.write("----- Test 1 -----\nTest Passed: %r\n" % json_equals_dict) 
+
+# ==============================
+# TEST 2: Testing empty query 
+# ==============================
+
+# Create dummy data (dictionary)
+query_dict = {
+    "returnCode": None,
+    "error": None,
+    "documents": [],
+    "tokens": []
+}
+
+json_query = query_dict
+
+#Pass it into the parsing method
+parse_query_result = parseJsonFromIndex(json_query)
+
+#Compare output to original input dictionary to make sure they are the same
+json_equals_dict = query_dict == parse_query_result
+
+#Write pass/fail results to output file
+f.write("----- Test 2 -----\nTest Passed: %r\n" % json_equals_dict) 
+
+# ==============================
+# TEST 3: Testing empty input 
+# ==============================
+
+# Create dummy data (dictionary)
+query_dict = None
+
+#Pass it into the parsing method
+parse_query_result = parseJsonFromIndex(query_dict)
+
+#Compare output to original input dictionary to make sure they are the same
+json_equals_dict = None == parse_query_result
+
+#Write pass/fail results to output file
+f.write("----- Test 3 -----\nTest Passed: %r\n" % json_equals_dict) 
 
 f.close()
