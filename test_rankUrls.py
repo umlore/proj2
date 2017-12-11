a2ce696a36cd297598b29724b7c857091bdf28f2
jfyan <jfyan008@gmail.com>
2017-12-10 20:47:47 -0500

    added scripts for testing rankUrls

---
+++ test_rankUrls.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+import requests
+import json
+
+#Import parsing functions to test
+from rank import rankUrls
+
+import sys
+import os
+sys.path.append(os.path.abspath('./rankURL_test'))
+from rankURL_test1 import queries, page_rank, inverted_index
+
+
+sorted_adocs = rankUrls(queries, page_rank, inverted_index)
+print(sorted_adocs)
